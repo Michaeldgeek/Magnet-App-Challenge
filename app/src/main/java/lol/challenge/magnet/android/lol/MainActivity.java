5f1fbb0365b0878e5e785c10c3f9a5dd6be24586
Michaeldgeek <petermichael908@gmail.com>
2015-09-29 01:55:27 +0100

    Application Structure

--- app/src/main/java/lol/challenge/magnet/android/lol/MainActivity.java
+++ app/src/main/java/lol/challenge/magnet/android/lol/MainActivity.java
@@ -1,16 +1,52 @@
 package lol.challenge.magnet.android.lol;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import net.yanzm.mth.MaterialTabHost;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setElevation(0);
+        }
+        MaterialTabHost tabHost = (MaterialTabHost) findViewById(android.R.id.tabhost);
+        tabHost.setType(MaterialTabHost.Type.FullScreenWidth);
+        SectionsPagerAdapter pagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+        for (int i = 0; i < pagerAdapter.getCount(); i++) {
+            tabHost.addTab(pagerAdapter.getPageTitle(i));
+        }
+
+        final ViewPager viewPager = (ViewPager) findViewById(R.id.pager);
+        viewPager.setAdapter(pagerAdapter);
+        viewPager.setOnPageChangeListener(tabHost);
+        viewPager.setCurrentItem(1);
+
+        tabHost.setOnTabChangeListener(new MaterialTabHost.OnTabChangeListener() {
+            @Override
+            public void onTabSelected(int position) {
+                viewPager.setCurrentItem(position);
+            }
+        });
+
     }
 
     @Override
@@ -33,5 +69,77 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a PlaceholderFragment (defined as a static inner class below).
+            return PlaceholderFragment.newInstance(position + 1);
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+                case 0:
+                    return getString(R.string.title_facebook).toUpperCase(l);
+                case 1:
+                    return getString(R.string.title_chats).toUpperCase(l);
+                case 2:
+                    return getString(R.string.title_find_friends).toUpperCase(l);
+            }
+            return null;
+        }
+    }
+
+    public static class PlaceholderFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static PlaceholderFragment newInstance(int sectionNumber) {
+            PlaceholderFragment fragment = new PlaceholderFragment();
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView;
+            if (getArguments().getInt(ARG_SECTION_NUMBER)== 1) {
+                // Facebook Fragment
+                rootView = inflater.inflate(R.layout.pager_facebook, container, false);
+                return rootView;
+            }
+            else {
+                rootView = inflater.inflate(R.layout.pager_fragment, container, false);
+                return rootView;
+            }
+        }
     }
 }
